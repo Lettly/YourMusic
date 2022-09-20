816d388036891a64be4d30d9ddd218c16cf5e9ff
Lorenzo Chiaese <lorenzochiaese@mc-adastra.it>
2022-09-20 12:42:45 +0200

    Update

--- api/v1/old/getDeviceInfo.php
+++ api/v1/old/getDeviceInfo.php
@@ -1,23 +1,29 @@
-
 <?php
-    require "../../include/config.php";
-    require "../../vendor/autoload.php";
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require $_SERVER["DOCUMENT_ROOT"].'/vendor/autoload.php';
 
     $session = new SpotifyWebAPI\Session(
-        $spotify_client_id,
-        $spotify_client_secret,
+        '37d6c9873a1b43ba94ae81466803dcb7',
+        '5c9c46d909fe4046836d52ddc5315373',
     );
 
 
-	
+    $servername = "217.182.206.163";
+    $username = "admin";
+    $password = "Andro.2001";
+    $dbname = "yourmusic";
+
     // Create connection
-    $conn = new mysqli($mysql_ip, $mysql_username, $mysql_password, $mysql_dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT refresh_token, token FROM opzioni";
+    $sql = "SELECT refresh_token, token, volume FROM opzioni";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -25,6 +31,7 @@
     while($row = $result->fetch_assoc()) {
         $accessToken = $row["token"];
         $refreshToken = $row["refresh_token"];
+        $volume = $row['volume'];
     }
     } else {
     echo "0 results";
@@ -49,7 +56,18 @@
 
     $api = new SpotifyWebAPI\SpotifyWebAPI($options, $session);
 
-    // Fetch the saved access token from somewhere. A database for example.
-    
-    // hot hits italia playlist
-    var_dump($api->play(false, ['context_uri' => 'spotify:playlist:37i9dQZEVXbIQnj7RRhdSX',]));
+    $volume = $api->getMyCurrentPlaybackInfo()->device->volume_percent;
+
+    $volume += $_GET['v'];
+    if ($volume > 100) {
+        $volume = 100;
+    } else if ($volume < 0) {
+        $volume = 0;
+    }
+       
+    // update song volume
+    // echo '<pre>';
+    // var_dump($api->getMyCurrentPlaybackInfo());
+
+    // var_dump($api->getMyCurrentPlaybackInfo() -> device -> volume_percent);
+
--- api/v1/old/volumeUpSong.php
+++ api/v1/old/volumeUpSong.php
@@ -1,23 +1,29 @@
-
 <?php
-    require "../../include/config.php";
-    require "../../vendor/autoload.php";
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require $_SERVER["DOCUMENT_ROOT"].'/vendor/autoload.php';
 
     $session = new SpotifyWebAPI\Session(
-        $spotify_client_id,
-        $spotify_client_secret,
+        '37d6c9873a1b43ba94ae81466803dcb7',
+        '5c9c46d909fe4046836d52ddc5315373',
     );
 
 
-	
+    $servername = "217.182.206.163";
+    $username = "admin";
+    $password = "Andro.2001";
+    $dbname = "yourmusic";
+
     // Create connection
-    $conn = new mysqli($mysql_ip, $mysql_username, $mysql_password, $mysql_dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT refresh_token, token FROM opzioni";
+    $sql = "SELECT refresh_token, token, volume FROM opzioni";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -25,6 +31,7 @@
     while($row = $result->fetch_assoc()) {
         $accessToken = $row["token"];
         $refreshToken = $row["refresh_token"];
+        $volume = $row['volume'];
     }
     } else {
     echo "0 results";
@@ -52,4 +59,4 @@
     // Fetch the saved access token from somewhere. A database for example.
     
     // hot hits italia playlist
-    var_dump($api->play(false, ['context_uri' => 'spotify:playlist:37i9dQZEVXbIQnj7RRhdSX',]));
+    var_dump($api->changeVolume(['volume_percent' => $volume+$_GET['v'],]));
